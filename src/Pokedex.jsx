7aa6717147ad48d3c9205f3875787e9e4d81360c
Raul Farias S. <ra.farias.s@outlook.com>
2025-08-09 09:49:56 -0400

    feat: add base TanStack Query configuration

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,13 +1,32 @@
 import { StrictMode, Suspense } from 'react'
 import { RouterProvider } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { router } from './routers/AppRouter'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      retryDelay: 500,
+      staleTime: 1000 * 60 * 60,
+      cacheTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false
+    }
+  }
+})
 
 const Pokedex = () => {
   return (
     <StrictMode>
-      <Suspense fallback={null}>
-        <RouterProvider router={router} />
-      </Suspense>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={null}>
+          <RouterProvider router={router} />
+        </Suspense>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StrictMode>
   )
 }
