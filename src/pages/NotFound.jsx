07e80458659ba5531c7431f56ab0e79f05eb45ce
Raul Farias S. <ra.farias.s@outlook.com>
2025-08-09 21:37:14 -0400

    fix: fix cursor behavior

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,9 +10,9 @@ const NotFound = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='grid h-[100dvh] grid-rows-[auto_1fr_auto] bg-[radial-gradient(circle_at_center,_white,_#fef3c7)]'>
+    <div className='grid min-h-[100dvh] grid-rows-[auto_1fr_auto] bg-[radial-gradient(circle_at_center,_white,_#fef3c7)]'>
       <PokeHeader />
-      <PokeMain className='flex-col gap-8 md:w-1/2 overflow-y-visible px-4'>
+      <PokeMain className='flex-col gap-8 md:w-1/2 px-4'>
         <img src={notFoundImage} className='drop-shadow-[0_0_15px_#0a0a0a3b] transition-transform duration-300 hover:scale-110 cursor-pikachu' alt='Ups... no pudimos encontrar esta página' draggable='false' />
         <p className='text-lg text-gray-700 font-medium text-center leading-[2]'>
           Parece que el huevo intentó eclosionar… ¡pero en lugar de un Pokémon, salió humo! Intenta más tarde o sigue tus huellas para no perder el rumbo y seguir explorando sin dejar atrás la diversión.
--- src/shared/components/PokeMain.jsx
+++ src/shared/components/PokeMain.jsx
@@ -1,6 +1,6 @@
 const PokeMain = ({ children, className }) => {
   return (
-    <main className={`overflow-y-auto px-2 my-15 mx-auto flex items-center justify-center ${className}`}>
+    <main className={`px-2 my-15 mx-auto flex items-center justify-center ${className}`}>
       {children}
     </main>
   )
