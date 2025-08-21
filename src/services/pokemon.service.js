6dc3d79d11102450af9bafb3ec7b1a7c12f07c2a
Raul Farias S. <ra.farias.s@outlook.com>
2025-08-21 00:59:30 -0400

    feat: enhance Pokémon search logic by adding evolutionary chain and lore

--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -26,18 +26,53 @@ export const pokeCategories = async () => {
 }
 
 export const pokemonDetails = async (id) => {
-  const { data } = await axios.get(`/pokemon/${id}`)
+  const { data: pokemon } = await axios.get(`/pokemon/${id}`)
+  const species = await pokemonSpecies(id)
+
   return {
-    id: data?.id,
-    name: data?.name,
-    height: data?.height,
-    weight: data?.weight,
-    abilities: data?.abilities,
-    stats: formatPokemonStats(data?.stats),
-    types: data?.types,
+    id: pokemon?.id,
+    name: pokemon?.name,
+    height: pokemon?.height,
+    weight: pokemon?.weight,
+    abilities: pokemon?.abilities,
+    stats: formatPokemonStats(pokemon?.stats),
+    types: pokemon?.types,
     sprites: {
-      default: data?.sprites?.other?.['official-artwork']?.front_default,
-      shiny: data?.sprites?.other?.['official-artwork']?.front_shiny
+      default: pokemon?.sprites?.other?.['official-artwork']?.front_default,
+      shiny: pokemon?.sprites?.other?.['official-artwork']?.front_shiny
+    },
+    lores: species.lores,
+    evochain: species.evochain
+  }
+}
+
+export const pokemonSpecies = async (id) => {
+  try {
+    const { data: species } = await axios.get(`/pokemon-species/${id}`)
+
+    return {
+      lores: [
+        ...new Set(
+          species?.flavor_text_entries?.flatMap((lore) => (
+            lore?.language?.name === 'es'
+              ? lore?.flavor_text?.replace(/\f/g, ' ').replace(/\n/g, ' ').trim() || []
+              : []
+          ))
+        )
+      ],
+      evochain: {
+        id: species?.evolution_chain?.url?.split('/').filter(Boolean).pop(),
+        chain: []
+      }
+    }
+  } catch (error) {
+    console.error(error.message)
+    return {
+      lores: [],
+      evochain: {
+        id: null,
+        chain: []
+      }
     }
   }
 }
@@ -64,4 +99,29 @@ export const pokemonsType = async (type) => {
       ?.map(({ pokemon }) => pokemon?.url?.split('/').filter(Boolean).pop())
       ?.sort((a, b) => a - b)
   }
+}
+
+export const pokemonGetEvolutionChain = async (id) => {
+  const { data: evoData } = await axios.get(`/evolution-chain/${id}`)
+
+  const chain = []
+
+  const traverse = async (node) => {
+    const { data: poke } = await axios.get(`/pokemon/${node.species.name}`)
+
+    chain.push({
+      id: poke?.id,
+      name: poke?.name,
+      sprites: {
+        default: poke?.sprites?.other?.['official-artwork']?.front_default,
+        shiny: poke?.sprites?.other?.['official-artwork']?.front_shiny
+      }
+    })
+
+    await Promise.all(node?.evolves_to?.map(traverse))
+  }
+
+  await traverse(evoData?.chain)
+
+  return { evolution_chain: chain }
 }
