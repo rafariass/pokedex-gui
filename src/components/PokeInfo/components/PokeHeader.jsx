81fa41fd9993df121148f887cede1652cb75982b
Raul Farias S. <ra.farias.s@outlook.com>
2025-08-21 01:08:22 -0400

    fix: fix pagination gap between intervals 1026-10001

--- src/components/PokeInfo/components/PokeHeader.jsx
+++ src/components/PokeInfo/components/PokeHeader.jsx
@@ -14,11 +14,11 @@ const PokeHeader = ({ name, id }) => {
   return (
     <div className='poke-info-heder'>
       <div className='w-full flex justify-end items-center gap-5'>
-        <Link to={`/pokemons/pokemon/${id - 1}`} className='poke-info-id poke-icon-border' style={previous}>
+        <Link to={`/pokemons/pokemon/${id === 10001 ? 1025 : id - 1}`} className='poke-info-id poke-icon-border' style={previous}>
           <ChevronLeftIcon className='poke-info-icon' />
         </Link>
         <span className='poke-info-id'>{formatCode(id)}</span>
-        <Link to={`/pokemons/pokemon/${id + 1}`} className='poke-info-id poke-icon-border' style={next}>
+        <Link to={`/pokemons/pokemon/${id === 1025 ? 10001 : id + 1}`} className='poke-info-id poke-icon-border' style={next}>
           <ChevronRightIcon className='poke-info-icon' />
         </Link>
       </div>
